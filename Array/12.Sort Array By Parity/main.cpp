8ff14e0804407467d537e3f7c838311361a54876
Dev-LinhPham <87310137+Dev-LinhPham@users.noreply.github.com>
2026-03-27 06:47:29 +0700

    [27022026][Array] Upload 12

---
+++ Array/12.Sort Array By Parity/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> sortArrayByParity(vector<int>& nums) {
+        int evenIndex = 0;
+        
+        for(int i = 0; i < nums.size(); ++i)
+        {
+            if(nums[i] % 2 == 0)
+                swap(nums[i], nums[evenIndex++]);
+        }
+        
+        return nums;
+    }
+};
